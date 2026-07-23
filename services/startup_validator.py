e36e34e02e840c91220413e8574b0243e85ff0d0
Preet <preetham.praneeth@gmail.comm>
2026-07-24 01:17:16 +0530

    Phase 12.1.1 - Add startup validator

---
+++ services/startup_validator.py
@@ -0,0 +1,8 @@
+class StartupValidator:
+    """
+    Validates application startup requirements before
+    backtesting or paper trading begins.
+    """
+
+    def validate(self):
+        return True
